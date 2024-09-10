faae91b85c2d70fa9243cfae329be810dd2074d5
PebPeb <67885698+PebPeb@users.noreply.github.com>
2024-09-09 23:30:33 -0400

    Config to link notes to sphinx build directory

--- sphinx/README.md
+++ sphinx/README.md
@@ -1,1 +1,8 @@
 place sphinx implementation here
+
+``` bash
+pip install sphinx
+pip install sphinx-rtd-theme
+pip install recommonmark
+pip install sphinx_markdown_tables
+```
--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -4,6 +4,31 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html
 
 import sphinx_rtd_theme
+import recommonmark
+from recommonmark.transform import AutoStructify
+import os
+
+
+target_dir = './../../notes'
+symlink_path = './notes'
+
+try:
+    # Create a symbolic link
+    os.symlink(target_dir, symlink_path)
+    print(f"Symbolic link created from {symlink_path} to {target_dir}")
+except FileExistsError:
+    print(f"Error: The symbolic link already exists at {symlink_path}")
+except FileNotFoundError:
+    print(f"Error: The target directory does not exist at {target_dir}")
+except PermissionError:
+    print(f"Error: Permission denied while creating the symbolic link")
+except OSError as e:
+    print(f"Error: {e}")
+
+
+# ============================================================================
+# Sphinx Implimentation
+# ============================================================================
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
@@ -16,13 +41,19 @@ author = 'Bryce Keen'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
 extensions = [
-    "sphinx_rtd_theme"
+    "sphinx_rtd_theme",
+    'recommonmark',
+    'sphinx_markdown_tables',
 ]
+
+source_suffix = ['.rst', '.md']
 
 templates_path = ['_templates']
 exclude_patterns = []
 
-
+# Auto-structure Markdown files
+def setup(app):
+    app.add_transform(AutoStructify)
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
